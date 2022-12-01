a978ecc0c8a406c8c9e828b05de6c69c9d69674e
Facundo Merkin <facundo.merkin.01@gmail.com>
2022-12-01 18:50:46 -0300

    NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+export const NavBar = () => {
+    return(
+        <nav class="nav">
+        <a href="index.html" aria-label="Home">MaxiKiosco</a>
+        <ul>
+            <li><button aria-label="Combos">Combos</button>
+            </li>
+            <li><button
+                    aria-label="Bebidas">Bebidas</button></li>
+            <li>
+                <button  aria-label="toggle light/dark mode">Comida</button>
+            </li>
+        </ul>
+    </nav>
+    )
+}
+
+export default NavBar
+
